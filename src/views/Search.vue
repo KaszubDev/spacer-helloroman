7f6d921c00aee1d10714611f0273212f4de84810
Krzysztof <kaszubowskikrzys@gmail.com>
2019-02-28 01:58:02 +0100

    Changes

---
+++ src/views/Search.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+    <div class="search">
+        <input name="search" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+};
+</script>
+<style lang="scss" scoped>
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        margin: 0;
+        padding: 30px;
+    }
+</style>
