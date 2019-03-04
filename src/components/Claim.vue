245141e12947ae1b46e88b71c8900e71874bb2d3
Krzysztof <kaszubowskikrzys@gmail.com>
2019-03-04 15:25:48 +0100

    Add basic hero image view

---
+++ src/components/Claim.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="claimWrapper">
+    <h1 class="claim">SPACER</h1>
+    <p class="subclaim">
+        Adsadadadadscascasdasdasdasdada
+    </p>
+    <p class="subclaim">
+        adadasdsadassafasdasdadasd
+    </p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Claim',
+};
+</script>
+<style lang="scss" scoped>
+    .claimWrapper {
+        display: flex;
+        text-align: center;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    .claim {
+        font-size: 50px;
+        margin: 0;
+    }
+    .subclaim {
+        margin: 20px 0 0 0;
+        font-size: 13px;
+    }
+</style>
